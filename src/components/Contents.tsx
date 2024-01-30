98097b17f801177b3692843aadcf30f3f2ac9a36
gwonminji <blueelf1124@gmail.com>
2024-01-30 17:06:54 +0900

    console.log

--- src/components/Contents.tsx
+++ src/components/Contents.tsx
@@ -19,6 +19,7 @@ const SubContentsWrap = styled.main`
 const Contents = ({ children }: WrapperProps) => {
   const location = useLocation();
   const root = location.pathname === '/tntn';
+  console.log(location);
 
   return (
     <>
